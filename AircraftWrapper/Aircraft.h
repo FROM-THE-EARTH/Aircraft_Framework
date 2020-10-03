c2a461c09734868e642cc3c93a0d914f02cce689
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2020-10-03 21:35:18 +0900

    Fix

--- AircraftWrapper/Aircraft.h
+++
@@ -1,9 +0,0 @@
-#include "../PlatformDefinition.h"
-
-#ifdef PLATFORM_MBED
-#include "AircraftMbed/AircraftMbed.h"
-#endif
-
-#ifdef PLATFORM_ARDUINO
-#include "AircraftArduino/AircraftArduino.h"
-#endif
